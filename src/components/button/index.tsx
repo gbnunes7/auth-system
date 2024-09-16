321c1075d113fbaa05b5a6cd92b8614ed4dfaf28
Gabriel Melo <bielrj170@gmail.com>
2024-09-15 23:27:27 -0300

    style: add a hover style to burron

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Button = ({type,children} : {type: "button" | "submit" | "reset", children: React.ReactNode}) => {
     return (
-        <button type={type} className="min-w-96 min-h-12 border rounded-lg text-center border-[#5A5A5A] text-lg text-[#5A5A5A]">
+        <button type={type} className="min-w-96 min-h-12 border rounded-lg text-center border-[#5A5A5A] text-lg text-[#5A5A5A] transition-transform duration-300 hover:scale-105">
             {children}
         </button>
     )
