e15da53cf74b7a35fff60ae6a6760deea794db2d
Gabriel Melo <bielrj170@gmail.com>
2024-09-16 22:47:57 -0300

    feat: add a methods to input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,15 +1,27 @@
 const Input = ({
 	placeholder,
 	type,
+	value,
+	onChange,
+	pattern,
+	required,
 }: {
 	placeholder: string;
 	type: string;
+	value: string;
+	pattern?: string;
+	required?: boolean;
+	onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }) => {
 	return (
 		<input
 			className="min-w-96 min-h-16 border rounded-lg border-[#D9D9D9] text-sm p-4 text-[#5A5A5A] placeholder:text-[#5A5A5A] focus:border-transparent focus:ring-0 focus:border-[#B0B0B0] focus:outline-none"
 			placeholder={placeholder}
 			type={type}
+			value={value}
+			pattern={pattern}
+			onChange={onChange}
+			required = {required}
 		/>
 	);
 };
