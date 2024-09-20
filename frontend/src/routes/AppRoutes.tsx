8969e94b34a32c0ea93dcf8d5e505c24006aa9b8
Gabriel Melo <bielrj170@gmail.com>
2024-09-20 18:18:17 -0300

    fix: delete obsolet files

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/login";
 import Signup from "../pages/signup";
 import FormProvider from "../context/formContext";
-import Home from "../pages/home";
 
 function AppRoutes() {
 	return (
@@ -11,7 +10,6 @@ function AppRoutes() {
 				<Routes>
 					<Route path="/" element={<Login />} />
 					<Route path="/signup" element={<Signup />} />
-					<Route path="/home" element={<Home />} />
 				</Routes>
 			</FormProvider>
 		</BrowserRouter>
--- src/components/containerImg/index.tsx
+++
@@ -1,8 +0,0 @@
-const ContainerImg = ({ bgUrl }: { bgUrl:string }) => {
-	return (
-		<div className="w-3/4 bg-center bg-contain bg-cover" style={{ backgroundImage: `url(${bgUrl})` }}>
-		</div>
-	);
-};
-
-export default ContainerImg;
