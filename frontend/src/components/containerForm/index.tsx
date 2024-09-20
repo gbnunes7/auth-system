13b9a8998c7bcb40cb29321c680970818611d511
Gabriel Melo <bielrj170@gmail.com>
2024-09-19 22:12:05 -0300

    fix: delete obsolet files

--- frontend/src/components/containerForm/index.tsx
+++
@@ -1,9 +0,0 @@
-const ContainerForm = ({children} : {children: React.ReactNode}) => {
-    return (
-        <div className="w-2/4 flex flex-col justify-center items-center">
-            {children}
-        </div>
-    )
-}
-
-export default ContainerForm
--- frontend/src/components/title/index.tsx
+++
@@ -1,7 +0,0 @@
-const Title = ({children} : {children:React.ReactNode}) => {
-	return (
-        <div className="text-black text-4xl font-semibold"> {children}</div>
-    );
-};
-
-export default Title
