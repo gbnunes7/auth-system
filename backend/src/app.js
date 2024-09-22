b9b2f5efff39fb2933c66499e76df8d20406a075
Gabriel Melo <bielrj170@gmail.com>
2024-09-21 21:20:50 -0300

    feat: use routes in app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 import express from "express"
 import connectToDb from "./database/config/connect.js"
+import routes from "./routes/index.js"
 
 export const app = express()
 
 connectToDb()
+
+routes(app)
