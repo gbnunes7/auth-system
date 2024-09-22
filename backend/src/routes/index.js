b4228921f3db33984bc50445e7c97ea5503bd526
Gabriel Melo <bielrj170@gmail.com>
2024-09-21 23:12:42 -0300

    feat: use cors middleware

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import users from "./userRoute.js";
 import auth from "../middleware/auth.js";
 import basic from "./userSignupRoute.js"
+import cors from "cors"
 
 const routes = (app) => {
 	app
 		.route("/")
 		.get((req, res) => res.status(200).json({ titulo: "AuthSystem" }));
 	app.use(express.json());
+	app.use(cors())
     app.use(basic)
     app.use(auth)
     app.use(users)
