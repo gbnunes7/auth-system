619a17530a9607f041d6c1a9887cb32f41db13d8
Gabriel Melo <bielrj170@gmail.com>
2024-09-22 01:15:23 -0300

    feat: add a button to logout

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,13 +1,17 @@
+import { CiLogout } from "react-icons/ci";
 import Title from "../../components/title";
 import useForm from "../../hooks/useFormContext";
 
 const Home = () => {
-	const { user } = useForm();
+	const { user, onLogout } = useForm();
 
 	return (
 		<>
-			<header className="w-full max-h-24 bg-[#E5E5E5] px-4">
-				<img src="./authLogo.png" alt="Imagem da logo" className="w-32 relative bottom-3" />
+			<header className="w-full max-h-24 bg-[#E5E5E5] px-4 flex justify-between items-center">
+				<img src="./authLogo.png" alt="Imagem da logo" className="w-32" />
+				<button onClick={onLogout}>
+					<CiLogout className="text-4xl" />
+				</button>
 			</header>
 			<div className="flex justify-center items-center py-4 px-4">
 				<Title>Congrats, {user} you're logged in</Title>
